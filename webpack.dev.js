43f4de3711a51692b93d59a403cf9e466c483535
Dima Lagutin <fsss70178@gmail.com>
2019-05-31 22:29:57 +0400

    init project

---
+++ webpack.dev.js
@@ -0,0 +1,68 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require('autoprefixer');
+const webpack = require("webpack");
+
+module.exports = merge(common, {
+		mode: 'development',
+		devServer: {
+				host: '0.0.0.0',
+			    port:3000,
+				publicPath: './src',
+				contentBase: path.resolve(__dirname, './src'),
+				watchContentBase: true,
+				writeToDisk: true,
+				compress: true,
+		},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.pug',
+			filename: './index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css"
+		}),
+		new webpack.LoaderOptionsPlugin({
+			options: {
+				postcss: [
+					autoprefixer()
+				]
+			}
+		})
+	],
+	module: {
+			rules: [
+				{
+					test: /\.pug$/,
+					use: ['pug-loader']
+				},
+				{
+					test: /\.(scss|css)$/,
+						use: [
+							{
+							loader: MiniCssExtractPlugin.loader,
+							options: {
+								hmr: process.env.NODE_ENV === 'development',
+								reloadAll: true
+							}
+						},
+							"css-loader",
+							"postcss-loader",
+							"sass-loader"
+
+						]
+				},
+				{
+					test: /\.tsx?$/,
+					loader: 'ts-loader',
+					exclude: /node_modules/,
+				}
+			]
+	},
+	//source map
+	devtool: 'cheap-module-eval-source-map'
+});
---
+++ webpack.prod.js
@@ -0,0 +1,54 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require('autoprefixer');
+const webpack = require("webpack");
+const CopyPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = merge(common, {
+	plugins: [
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css"
+		}),
+		new webpack.LoaderOptionsPlugin({
+			options: {
+				postcss: [
+					autoprefixer()
+				]
+			}
+		}),
+		new CopyPlugin([
+			{from: 'src/fonts', to: 'fonts'},
+			{from: 'src/*.html', to: path.resolve(__dirname, './build'),
+			flatten: true},
+			{from: 'src/files', to: path.resolve(__dirname, './build/files'),
+				flatten: true}
+		])
+	],
+	module: {
+		rules: [
+			{
+				test: /\.pug$/,
+				use: ['pug-loader']
+			},
+			{
+				test: /\.scss|css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"postcss-loader",
+					"sass-loader"
+				]
+			}
+		]
+	},
+		output: {
+				path: path.resolve(__dirname, './build'),
+				filename: 'ts/bundle.js'
+		},
+		mode: 'production',
+});
